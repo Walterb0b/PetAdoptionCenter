8467c3d9685e5d9e43c5a8434fb9ff4eaf77a340
linuxcompas <kalikeylogger00@gmail.com>
2025-09-15 13:49:16 +0200

    Cat Override from superclass

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,22 @@
-public class Cat {
+public class Cat extends Pet {
+
+    private String name;
+    private String type;
+
+    Cat(String type, String name) {
+        super(name, type);
+    }
+
+
+    @Override
+    public void sleep() {
+        super.sleep();
+    }
+        public void speak() {
+            super.speak();
+        }
+        public void move() {
+        super.move();
+        
+    }
 }
