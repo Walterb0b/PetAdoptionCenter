7fb1d766aecee9ee14dc6eb724b8de64dc53e6e5
Walterb0b <osca0001@stud.ek.dk>
2025-09-15 13:48:43 +0200

    test class

--- src/AdoptionCenter.java
+++ src/AdoptionCenter.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class AdoptionCenter {
+    public static void main(String[] args) {
+
+    }
+
+    public static void adoptioncenter(){
+        //Scanner for user inputs
+        Scanner sc = new Scanner(System.in);
+
+        //Welcome message
+        System.out.println("Hello and welcome to the adoption center!");
+        System.out.print("How many pets would you like to register: ");
+        int numberOfPets = sc.nextInt();
+        sc.next();
+
+
+
+    }
 }
