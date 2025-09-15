68724e4b6e6a59a1312e79e22788c411241c762a
Walterb0b <osca0001@stud.ek.dk>
2025-09-15 13:20:14 +0200

    constructor, getters, setters

--- src/Pet.java
+++ src/Pet.java
@@ -1,2 +1,22 @@
 public class Pet {
+    private String name;
+    private String type;
+
+
+    //Constructor
+    Pet(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    //Getters
+    public String getName() {return name;}
+    public String getType() {return type;}
+
+    //Setters
+    public void setName(String name) {this.name = name;}
+    public void setType(String type) {this.type = type;}
+
+
+
 }
